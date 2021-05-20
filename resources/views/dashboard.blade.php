65441d5ffc4e0c57a83b13cdb712213fc043b9d0
Bryan Jo <bryantjipto11@gmail.com>
2021-05-20 20:46:54 +0700

    updated user frontend

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,43 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           Hi..{{ Auth::user()->name }}
-        </h2>
-    </x-slot>
+@extends('frontend.main_master')
+@section('content')
+    
+<div class="body-content">
+	<div class="container">
+			<div class="row">
+				<div class="col-md-2"><br>
+					<img src="{{(!empty($user->profile_photo_path))? 
+						url('upload/user_images/'.$user->profile_photo_path):url('upload/no_image.jpg')}}" class="card-img-top" style="border-radius:50%" 
+						width="100%" height="100%" alt=""><br><br>
+						<ul class="list-group list-group-flush">
+							<a href="" class="btn btn-primary btn-sm btn-block">Home</a>
+							<a href="{{route('user.profile')}}" class="btn btn-primary btn-sm btn-block">Profile Update</a>
+							<a href="{{route('change.password')}}" class="btn btn-primary btn-sm btn-block">Change Password</a>
+							<a href="{{route('user.logout')}}" class="btn btn-danger btn-sm btn-block">Logout</a>
+						</ul>
+				</div>
 
-    <div class="py-12">
-        This is just home page 
-    </div>
-</x-app-layout>
+				<div class="col-md-2">
+					
+				</div>
+
+				<div class="col-md-6">
+					<div class="card">
+						<h3 class="text-center"><span class="text-danger">Hi...</span><strong>{{Auth::user()->name}}</strong> 
+							Welcome to KulinerKita</h3>
+					</div>
+				</div>
+			</div>
+	</div>
+
+
+	@include('frontend.body.brands')
+
+</div>
+
+
+
+
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\AdminProfileController;
+use App\Http\Controllers\Frontend\IndexController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +19,11 @@ use App\Http\Controllers\Backend\AdminProfileController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::group(['prefix'=> 'admin', 'middleware'=>['admin:admin']], function(){
 	Route::get('/login', [AdminController::class, 'loginForm']);
 	Route::post('/login',[AdminController::class, 'store'])->name('admin.login');
 });
-
 
 
 
@@ -37,10 +37,20 @@ Route::get('/admin/profile', [AdminProfileController::class, 'AdminProfile'])->n
 Route::get('/admin/profile/edit', [AdminProfileController::class, 'AdminProfileEdit'])->name('admin.profile.edit');
 Route::post('/admin/profile/store', [AdminProfileController::class, 'AdminProfileStore'])->name('admin.profile.store');
 Route::get('/admin/change/password', [AdminProfileController::class, 'AdminChangePassword'])->name('admin.change.password');
-
 Route::post('/update/change/password', [AdminProfileController::class, 'AdminUpdateChangePassword'])->name('update.change.password');
 
 
+
+// User All Routes 
 Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    $id = Auth::user()->id;
+    $user = User::find($id);
+    return view('dashboard',compact('user'));
 })->name('dashboard');
+
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/user/logout', [IndexController::class, 'UserLogout'])->name('user.logout');
+Route::get('/user/profile', [IndexController::class, 'UserProfile'])->name('user.profile');
+Route::post('/user/profile/store', [IndexController::class, 'UserProfileStore'])->name('user.profile.store');
+Route::get('/user/change/password', [IndexController::class, 'UserChangePassword'])->name('change.password');
+Route::post('/user/password/update', [IndexController::class, 'UserPasswordUpdate'])->name('user.password.update');
